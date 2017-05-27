f3b6ccc7dc1c25da3aa17966e556b3acfc58e338
david <vagercai@gmail.com>
2017-05-27 11:02:27 +0800

    chapter 8

--- src/com/company/Money.java
+++ src/com/company/Money.java
@@ -3,11 +3,20 @@ package com.company;
 /**
  * Created by david on 27/5/2017.
  */
-public class Money {
+abstract public class Money {
     protected int amount;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount && (this.getClass() == object.getClass());
     }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+    abstract Money times(int muliplier);
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -13,30 +13,31 @@ public class Test {
     }
     public void testMultiplication() {
         System.out.println("----------testMultiplication----------");
-        Dollar five = new Dollar(5);
-        AssertHelper.assertEquals( new Dollar(10), five.times(2));
-        AssertHelper.assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        AssertHelper.assertEquals( Money.dollar(10), five.times(2));
+        AssertHelper.assertEquals(Money.dollar(15), five.times(3));
     }
 
     public void testEquality() {
         System.out.println("----------testEquality----------");
-        AssertHelper.assertEquals(true, new Dollar(5).equals(new Dollar(5)));
-        AssertHelper.assertEquals(false, new Dollar(6).equals(new Dollar(5)));
+        AssertHelper.assertEquals(true, Money.dollar(5).equals(Money.dollar(5)));
+        AssertHelper.assertEquals(false, Money.dollar(6).equals(Money.dollar(5)));
     }
 
     public void testFrancMultiplication() {
         System.out.println("----------testFrancMultiplication----------");
-        Franc five = new Franc(5);
-        AssertHelper.assertEquals( new Franc(10), five.times(2));
-        AssertHelper.assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        AssertHelper.assertEquals( Money.franc(10), five.times(2));
+        AssertHelper.assertEquals(Money.franc(15), five.times(3));
     }
 
     public void testMoneyEquality() {
         System.out.println("----------testMoneyEquality----------");
-        AssertHelper.assertEquals(true, new Dollar(5).equals(new Dollar(5)));
-        AssertHelper.assertEquals(false, new Dollar(6).equals(new Dollar(5)));
-        AssertHelper.assertEquals(true, new Franc(5).equals(new Franc(5)));
-        AssertHelper.assertEquals(false, new Franc(6).equals(new Franc(5)));
-        AssertHelper.assertEquals(false, new Dollar(5).equals(new Franc(5)));
+        AssertHelper.assertEquals(true, Money.dollar(5).equals(Money.dollar(5)));
+        AssertHelper.assertEquals(false, Money.dollar(6).equals(Money.dollar(5)));
+        AssertHelper.assertEquals(true, Money.franc(5).equals(Money.franc(5)));
+        AssertHelper.assertEquals(false, Money.franc(6).equals(Money.franc(5)));
+        AssertHelper.assertEquals(false, Money.dollar(5).equals(Money.franc(5)));
     }
+
 }
